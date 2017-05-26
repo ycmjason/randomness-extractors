438773e5ee638ffaae25470bf3fb6f9c82621013
Jason Yu <me@ycmjason.com>
2017-05-26 01:28:52 +0100

    added more tests and fixed types errors

--- test/index.test.js
+++ test/index.test.js
@@ -10,10 +10,12 @@ describe('Randomness Extractor', function(){
       { input: ["00001", "11110"], expect: 0 },
       { input: ["00011", "11110"], expect: 1 },
       { input: ["00111", "11110"], expect: 0 },
+      { input: ["00111111", "11110"], expect: 0 },
+      { input: ["00111", "11110101001010"], expect: 0 },
     ];
     specs.forEach(spec => {
-      it(`# innerProductExtractor(${spec.input[0]}, ${spec.input[1]}) === ${spec.expect}`, function(){
-        assert.equal(Extractors.innerProductExtractor(spec.input[0].split(''), spec.input[1].split('')), spec.expect)
+      it(`# innerProductExtractor(${spec.input}) === ${spec.expect}`, function(){
+        assert.equal(Extractors.innerProductExtractor([spec.input[0].split(''), spec.input[1].split('')]), spec.expect)
       });
     });
   });
